b67467f42487c24c841f04c9051858199c739ff5
Alan Smillie <alan.smillie@gmail.com>
2019-06-20 11:38:05 -0400

    Generate component files for shopping list and recipe book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeListItemComponent } from './recipe-book/recipe-list-item/recipe-list-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListItemComponent } from './shopping/shopping-list-item/shopping-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeListItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListItemComponent
   ],
   imports: [
     BrowserModule
