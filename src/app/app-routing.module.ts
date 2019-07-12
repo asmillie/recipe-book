311e9ff01814a12635378c5c0ea4918d420c8051
Alan Smillie <alan.smillie@gmail.com>
2019-07-12 09:43:43 -0400

    Move routing to separate module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeListItemComponent } from './recipe-book/recipe-list-item/recipe-list-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
@@ -10,25 +9,23 @@ import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.co
 import { ShoppingListItemComponent } from './shopping/shopping-list-item/shopping-list-item.component';
 import { HeaderComponent } from './header/header.component';
 import { AddRecipeComponent } from './recipe-book/add-recipe/add-recipe.component';
-import { AppRoutingModule } from './app-routing.module';
+
+const appRoutes: Routes = [
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'recipe-book', component: RecipeListComponent },
+  { path: 'add-recipe', component: AddRecipeComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecipeListComponent,
-    RecipeListItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListItemComponent,
-    HeaderComponent,
-    AddRecipeComponent,
-  ],
+  declarations: [],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    RouterModule
+  ]
 })
-export class AppModule { }
+export class AppRoutingModule { }
