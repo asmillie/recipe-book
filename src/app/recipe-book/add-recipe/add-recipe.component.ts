aea42d46d6bd3926b4e024e750edec0d18992900
Alan Smillie <alan.smillie@gmail.com>
2019-08-08 11:11:19 -0400

    Remove validation on ingredients for testing

--- src/app/recipe-book/add-recipe/add-recipe.component.ts
+++ src/app/recipe-book/add-recipe/add-recipe.component.ts
@@ -107,7 +107,7 @@ export class AddRecipeComponent implements OnInit, OnDestroy {
         ingredientName,
         amount,
         unit
-      }, { validators: RequiredValidator })
+      })
     );
   }
 
