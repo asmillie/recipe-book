dc43b6164c478b10d7f6b92003737afe3ff75d88
Alan Smillie <alan.smillie@gmail.com>
2019-09-04 10:20:11 -0400

    Add user login for firebase

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { AuthService, IFirebaseAuthResponse } from './auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
   loginForm;
+  authSubscription: Subscription;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService) { }
 
   ngOnInit() {
     this.initForm();
   }
 
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+
+  // TODO: Add validation
   private initForm() {
     this.loginForm = this.fb.group({
       email: [''],
@@ -25,6 +35,15 @@ export class AuthComponent implements OnInit {
 
   onSubmit() {
     console.log(this.loginForm);
+    const email = this.loginForm.get('email').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authSubscription = this.auth.loginUser(email, password)
+      .subscribe((response: IFirebaseAuthResponse) => {
+        console.log(response);
+      }, (error) => {
+        console.log('Component error: ', error);
+      });
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export interface IFirebaseAuthResponse {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private FIREBASE_EMAIL_SIGN_IN_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+  private FIREBASE_EMAIL_SIGN_UP_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  private FIREBASE_WEB_API_KEY = 'AIzaSyBdqNa7ICSXkq-VZ0JG_L1mkwlw3ZgTjY0';
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(email: string, password: string): Observable<IFirebaseAuthResponse> {
+    return this.http.post<IFirebaseAuthResponse>(
+      this.FIREBASE_EMAIL_SIGN_IN_URL + this.FIREBASE_WEB_API_KEY,
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    ).pipe(
+      catchError((error: HttpErrorResponse, caught: Observable<IFirebaseAuthResponse>) => {
+        return this.handleLoginError(error, email, password);
+      })
+    );
+  }
+
+  createUser(email: string, password: string): Observable<IFirebaseAuthResponse> {
+    return this.http.post<IFirebaseAuthResponse>(
+      this.FIREBASE_EMAIL_SIGN_UP_URL + this.FIREBASE_WEB_API_KEY,
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    ).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleLoginError(
+    error: HttpErrorResponse,
+    email: string,
+    password: string): Observable<never> | Observable<IFirebaseAuthResponse> {
+    const errorMessage = error.error.error.message;
+    // TODO: Remove logging
+    console.error(error);
+    // If user is attempting login but doesn't have account then
+    // create user with provided email and password
+    if (errorMessage === 'EMAIL_NOT_FOUND') {
+      return this.createUser(email, password);
+    }
+    return throwError(`${error.error.error.message}`);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    // TODO: Remove logging
+    console.error(error);
+    return throwError(`${error.error.error.message}`);
+  }
 }
