137cc83e9d37f196612dcf9d090109b7faa127de
Alan Smillie <alan.smillie@gmail.com>
2019-09-06 10:46:36 -0400

    Hide links from unauthorized user

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  userSub: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initUserAuth();
   }
 
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  private initUserAuth() {
+    this.isLoggedIn = false;
+    this.userSub = this.authService.user.subscribe((user) => {
+      if (user.token !== null) {
+        return this.isLoggedIn = true;
+      }
+
+      this.isLoggedIn = false;
+    });
+
+  }
 }
