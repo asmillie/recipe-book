ec422b4819bb0cd9134f4e5e2e61922be6b4e921
Alan Smillie <alan.smillie@gmail.com>
2019-09-11 10:35:29 -0400

    Prevent ongoing subscription

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +19,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> {
       console.log('CanActivate');
       return this.authService.user.pipe(
+        take(1),
         map(user => {
           if (user && user.token) {
             return true;
