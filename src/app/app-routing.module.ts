f65973354d9518774caf8db28bb7f81a237e80d1
Alan Smillie <alan.smillie@gmail.com>
2019-09-27 14:35:46 -0400

    Optimize imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { AddRecipeComponent } from './recipe-book/add-recipe/add-recipe.component';
-import { RecipesComponent } from './recipe-book/recipes.component';
-import { RecipeDetailResolverService } from './recipe-book/recipe-detail/recipe-detail-resolver.service';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth/auth.guard';
 
